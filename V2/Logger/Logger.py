45273d73f9b26151a6fc8947edcfffef59d4c53b
Takatsuka-Mark <takatsuka.mark@gmail.com>
2020-02-04 16:10:04 -0500

    V2 Added more logging functionality and usage on server

---
+++ V2/Logger/Logger.py
@@ -0,0 +1,27 @@
+import datetime
+
+
+class Logger:
+    def __init__(self, instance_name):
+        self.instance_name = instance_name
+        self.log = open("../Logger/" + datetime.datetime.now().strftime("%m-%d-%y %H-%M-%S") + " " + str(instance_name) + ".txt", "w")
+
+    def close_logger(self):
+        self.log.close()
+
+    def write_connect(self, server_id, port):
+        self.__write_str(str(server_id) + " Connected on Port: " + str(port))
+
+    def write_disconnected(self, server_id, port):
+        self.__write_str(str(server_id) + " Disconnected from Port: " + str(port))
+
+    def write_recv(self, server_id, size):
+        self.__write_str(str(server_id) + " Received data " + str(size))
+
+    def write_send(self, server_id, size):
+        self.__write_str(str(server_id) + " Sending data " + str(size))
+
+    def __write_str(self, string):
+        self.log.writelines(datetime.datetime.now().strftime("%m-%d-%y %H-%M-%S") + "\t\t" + string + "\n")
+        self.log.flush()
+
--- V2/Server/Server.py
+++ V2/Server/Server.py
@@ -1,4 +1,5 @@
 from V2.NetworkingConstants import MAX_CONNECTIONS, IP, PORT
+from V2.Logger.Logger import Logger
 from V2.Server.ServerNetworking import ServerNetworking
 import threading
 import socket
@@ -6,20 +7,26 @@ import time
 
 
 class ServerThread(threading.Thread):
-    def __init__(self, ip, port, conn, exit_signal):
+    def __init__(self, ip, port, conn, exit_signal, logger, identification):
         super().__init__()
         self.net = ServerNetworking(conn, ip, port)
         self.exit_signal = exit_signal
+        self.logger = logger
+        self.identification = identification
         print("New Socket Thread Started For: {0}:{1}".format(str(ip), str(port)))
+        logger.write_connect(identification, ip)
+
 
     def run(self) -> None:
         while not self.exit_signal.is_set():
             data = self.net.receive_data()
             if data is None or not self.net.is_connected():
+                self.logger.write_disconnected(self.identification, self.net.port)
                 print("Client Disconnected")
                 exit(0)
 
             print("Received Data:", data)
+            self.logger.write_recv(self.identification, data)
             self.net.send_str("Received: " + data)
 
 
@@ -29,21 +36,24 @@ def main():
     server.bind((IP, PORT))
     threads = []
     exit_signal = threading.Event()
+    logger = Logger("Echo Server Test")
 
     try:
+        server_id = 0
+        server.listen(4)
         while len(threads) < MAX_CONNECTIONS:
-            server.listen(4)
             (conn, (ip, port)) = server.accept()
-
-            temp_thread = ServerThread(ip, port, conn, exit_signal)
+            temp_thread = ServerThread(ip, port, conn, exit_signal, logger, server_id)
             temp_thread.start()
-            threads.append(threads)
+            threads.append(temp_thread)
+            server_id += 1
         while True:
-            time.sleep(1)
+            time.sleep(.5)
 
     except KeyboardInterrupt:
         print("Keyboard Interrupt Received")
         exit_signal.set()
+        logger.close_logger()
         time.sleep(1)
 
     for t in threads:
